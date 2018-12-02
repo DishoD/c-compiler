fb592d87b200068cbef28834b3f51b31a18c4176
Dominik Batić <dominik.batic@fer.hr>
2018-12-02 21:11:21 +0000

    Upload New File

---
+++ lab2/src/DOMINIK/Akcija.java
@@ -0,0 +1,10 @@
+/**
+ * Sučelje koje određuje sve akcije(argumente) za određeni par stanja i regularnog izraza.
+ */
+public interface Akcija {
+    /**
+     * Izvedi akciju(argument).
+     */
+    void izvrsi();
+}
+
